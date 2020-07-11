86766bac1e261807b47dea40ec274a6cbfd91e56
Melissa Geels <spansjh@gmail.com>
2020-07-11 20:30:03 +0200

    New logging options

--- log/log.go
+++ log/log.go
@@ -1,49 +1,77 @@
 package log
 
 import (
-	"time"
 	"fmt"
+	"time"
+
 	"github.com/fatih/color"
 )
 
-type LogConfig struct {
-	MinLevel, MaxLevel int
+// Config contains instructions on how to write log to stdout
+type Config struct {
+	MinLevel        int
+	MaxLevel        int
+	Timestamp       bool
+	TimestampFormat string
+	Category        bool
+	CategoryFormat  string
 }
 
-var Config = LogConfig{
-	CatTrace,
-	CatFatal,
+// CurrentConfig is the configuration currently used for logging
+var CurrentConfig = Config{
+	MinLevel:  CatTrace,
+	MaxLevel:  CatFatal,
+	Timestamp: true,
+	Category:  true,
 }
 
 const (
+	// CatTrace is the level for trace messages
 	CatTrace = iota
+	// CatDebug is the level for debug messages
 	CatDebug
+	// CatInfo is the level for info messages
 	CatInfo
+	// CatWarn is the level for warn messages
 	CatWarn
+	// CatError is the level for error messages
 	CatError
+	// CatFatal is the level for fatal messages
 	CatFatal
 )
 
 func nameForCategory(cat int) string {
 	switch cat {
-	case CatTrace: return "TRACE"
-	case CatDebug: return "DEBUG"
-	case CatInfo: return "INFO"
-	case CatWarn: return "WARN"
-	case CatError: return "ERROR"
-	case CatFatal: return "FATAL"
+	case CatTrace:
+		return "TRACE"
+	case CatDebug:
+		return "DEBUG"
+	case CatInfo:
+		return "INFO"
+	case CatWarn:
+		return "WARN"
+	case CatError:
+		return "ERROR"
+	case CatFatal:
+		return "FATAL"
 	}
 	return "???"
 }
 
 func colorForCategory(cat int) color.Attribute {
 	switch cat {
-	case CatTrace: return color.FgWhite
-	case CatDebug: return color.FgHiCyan
-	case CatInfo: return color.FgHiWhite
-	case CatWarn: return color.FgHiMagenta
-	case CatError: return color.FgHiYellow
-	case CatFatal: return color.FgHiRed
+	case CatTrace:
+		return color.FgWhite
+	case CatDebug:
+		return color.FgHiCyan
+	case CatInfo:
+		return color.FgHiWhite
+	case CatWarn:
+		return color.FgHiMagenta
+	case CatError:
+		return color.FgHiYellow
+	case CatFatal:
+		return color.FgHiRed
 	}
 	return color.FgWhite
 }
@@ -53,47 +81,54 @@ func writeLine(cat int, s string, args ...interface{}) {
 		return
 	}
 
-	tmNow := time.Now()
-	tmFormatted := fmt.Sprintf("%02d:%02d:%02d.%03d", tmNow.Hour(), tmNow.Minute(), tmNow.Second(), tmNow.Nanosecond() / 1000000)
-	line := fmt.Sprintf(s, args...)
-
-	catName := nameForCategory(cat)
 	catColor := colorForCategory(cat)
-
 	color.Set(catColor)
-	fmt.Printf("[%s] [% 5s] %s\n", tmFormatted, catName, line)
+
+	if CurrentConfig.Timestamp {
+		tmNow := time.Now()
+		fmt.Printf("%02d:%02d:%02d.%03d | ", tmNow.Hour(), tmNow.Minute(), tmNow.Second(), tmNow.Nanosecond()/1000000)
+	}
+
+	if CurrentConfig.Category {
+		catName := nameForCategory(cat)
+		fmt.Printf("% 5s | ", catName)
+	}
+
 	color.Unset()
+
+	fmt.Printf(s+"\n", args...)
 }
 
 func passesFilter(cat int) bool {
-	return cat >= Config.MinLevel && cat <= Config.MaxLevel
+	return cat >= CurrentConfig.MinLevel && cat <= CurrentConfig.MaxLevel
 }
 
-func Open(minLevel, maxLevel int) {
-	Config.MinLevel = minLevel
-	Config.MaxLevel = maxLevel
-}
-
+// Trace log message
 func Trace(s string, args ...interface{}) {
 	writeLine(CatTrace, s, args...)
 }
 
+// Debug log message
 func Debug(s string, args ...interface{}) {
 	writeLine(CatDebug, s, args...)
 }
 
+// Info log message
 func Info(s string, args ...interface{}) {
 	writeLine(CatInfo, s, args...)
 }
 
+// Warn log message
 func Warn(s string, args ...interface{}) {
 	writeLine(CatWarn, s, args...)
 }
 
+// Error log message
 func Error(s string, args ...interface{}) {
 	writeLine(CatError, s, args...)
 }
 
+// Fatal log message
 func Fatal(s string, args ...interface{}) {
 	writeLine(CatFatal, s, args...)
 }
